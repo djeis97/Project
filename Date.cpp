b3e75d75e39be25c27ce3dbb231318f2e14c3e3c
Elijah Malaby <qwe12345678910@gmail.com>
2016-11-15 16:41:46 -0500

    Renamed print to operator<<.

--- Date.cpp
+++ Date.cpp
@@ -18,6 +18,6 @@ int Date::getYear () {
   return year;
 }
 
-std::ostream& Date::print (std::ostream& out) {
+std::ostream& Date::operator<< (std::ostream& out) {
   return out << day << "/" << month << "/" << year;
 }
--- Date.hpp
+++ Date.hpp
@@ -6,11 +6,10 @@ class Date
 {
 public:
   Date (int day, int month, int year);
-  virtual ~Date ();
   int getDay ();
   int getMonth ();
   int getYear ();
-  std::ostream& print (std::ostream& out);
+  std::ostream& operator<< (std::ostream& out);
 private:
   int day;
   int month;
