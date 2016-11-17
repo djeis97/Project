764242d40fccca97e3616b3a6ad6473310b17dc8
Elijah Malaby <qwe12345678910@gmail.com>
2016-11-17 16:18:44 -0500

    Added DataObject and Course.hpp stub

---
+++ Course.hpp
@@ -0,0 +1,12 @@
+#ifndef COURSE_H
+#define COURSE_H
+
+class Course
+{
+public:
+  Course();
+  virtual ~Course();
+};
+
+
+#endif /* COURSE_H */
---
+++ DataObject.hpp
@@ -0,0 +1,31 @@
+#ifndef DATAOBJECT_H
+#define DATAOBJECT_H
+
+#include "Person.hpp"
+#include "Department.hpp"
+#include "Course.hpp"
+
+#include <vector>
+
+class DataObject
+{
+public:
+  DataObject();
+  virtual ~DataObject();
+
+  Person* getPersonById (int id);
+  Department* getDepartmentById (int id);
+  Course* getCourseById (int id);
+
+  void addPerson (Person* newPerson);
+  void addDepartment (Department* newDepartment);
+  void addCourse (Course* newCourse);
+
+private:
+  std::vector<Person *> people;
+  std::vector<Department *> departments;
+  std::vector<Course *> courses;
+};
+
+
+#endif /* DATAOBJECT_H */
