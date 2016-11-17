d2b9f599f51a60addba3bd14d0d5c08d9981928f
Elijah Malaby <qwe12345678910@gmail.com>
2016-11-17 18:46:43 -0500

    Basic Course implementation.

--- Course.cpp
+++ Course.cpp
@@ -1,9 +1,15 @@
 #include "Course.hpp"
 
-Course::Course (int myId)
-  : courseId(myId) {
+#include <string.h>
+
+Course::Course (int myId, std::string myLevel)
+  : courseId(myId), level(myLevel) {
 }
 
 int Course::getCourseId () {
   return courseId;
+}
+
+int Course::getStudentGrade (int studentId) {
+  return grades.at(studentId);
 }
--- Course.hpp
+++ Course.hpp
@@ -3,19 +3,26 @@
 
 #include "DataObject.hpp"
 
+#include <map>
+#include <string.h>
+
 class DataObject;
 
 class Course
 {
 public:
-  Course (int myId);
+  Course (int myId, std::string myLevel);
   virtual ~Course ();
 
   int getCourseId ();
 
 private:
   int courseId;
+  std::string level;
+  std::map<int, int> grades; // Student IDs to Grades
   DataObject *dataObject;
+
+  int getStudentGrade (int studentId);
 };
 
 
